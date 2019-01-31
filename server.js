fc0e573befd58cf39e5a32031ef87d70b3001752
Mayja Tom <maja.morales17@gmail.com>
2019-01-31 15:02:06 -0800

    adding server file

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+// require("dotenv").config();
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const db = require("./models");
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+//passport
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
+
+app.use(passport.initialize());
+
+app.use(passport.session()); // persistent login sessions
+
+// Static directory
+//needs to be replaced with react index file
+app.use(express.static("public"));
+
+
+
+// Start the API server
+db.sequelize.sync({ force: false }).then(function () {
+    app.listen(PORT, function () {
+        console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    });
+  });
+
